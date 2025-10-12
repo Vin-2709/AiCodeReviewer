5c1636bc5af7541c70f9b3237d806d5e07d5184c
Vineet <vin.itku7277@gmail.com>
2025-10-13 00:58:10 +0530

    updated

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const app=require('./src/app')
+const PORT=process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log('Server is running ');
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
+
+// app.listen(3000,()=>{
+//     console.log('Server is running ');
+// })
